605f7f0e33ddc24dd82745a338c90ada65575e70
damonk@gmail.com <damonkd@gmail.com>
2019-07-28 03:28:47 -0700

    fixed port error

--- server.js
+++ server.js
@@ -152,7 +152,7 @@ app.post("/articles/:id", function(req, res) {
 });
 
 // Start the server
-app.listen(process.env.PORT || 3000, function() {
+app.listen(PORT, function() {
   console.log("App running on port " + PORT + "!");
 });
 module.exports = scraped;
